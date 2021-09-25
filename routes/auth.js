8550a9c14a684df829cb7294b0a5f1ffb7e43f3a
ShubhanshuRai27 <shubhanshu.rai94@gmail.com>
2021-09-25 23:06:58 +0530

    userSchema

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
 router.post("/register", (req, res) => {
-  res.send("Register");
+  const registerUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
 });
 
 router.post("/login", (req, res) => {
